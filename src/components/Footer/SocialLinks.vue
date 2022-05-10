694209a862d4885e1abf0f54efc9cb27c19495f2
Chrissle28 <christian.z.dev@gmail.com>
2022-05-10 10:30:32 +0200

    social links component

---
+++ src/components/Footer/SocialLinks.vue
@@ -0,0 +1,15 @@
+<template>
+    <!-- TODO: Check structure of social links later with real data -->
+    <div class="flex flex-wrap items-center gap-4">
+        <a target="_blank" href="" v-for="link in links"> Social Link Icon </a>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    links: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
